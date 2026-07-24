99456ef744af5705571265de02bebdf98aa44f64
setayesh1384 <setayesh.sedghi84@gmail.com>
2026-07-25 02:07:43 +0330

    update sun.py

---
+++ src/sun.py
@@ -0,0 +1,76 @@
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import streamlit as st
+from PIL import Image
+
+# Path: src/visualize.py
+
+login_option = st.sidebar.radio(
+    "Login/Signup",
+    ("Login", "Signup")
+)
+
+if login_option == "Login":
+    with st.sidebar.form("login"):
+        st.write("Login Here.")
+
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Login")
+
+        if submitted:
+            pass
+
+else:
+    with st.sidebar.form("signup"):
+        st.write("Signup Here.")
+
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        email = st.text_input("Email")
+
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Signup")
+
+        if submitted:
+            pass
+
+
+
+st.title(":zap: Pytopia Dashboard")
+
+# Metrics
+col1, col2 = st.columns(2)
+
+col1.metric(
+    label="Pytopia Telegram Members",
+    value="4800",
+    delta="+100"
+)
+
+col2.metric(
+    label="Pytopia Website Members",
+    value="2102",
+    delta="+10"
+)
+
+with st.expander("Statistics"):
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+    sns.histplot(np.random.randn(100), ax=ax)
+    st.pyplot(fig)
+
+with st.expander("User Profile:"):
+    col1, col2 = st.columns(2)
+
+    col1.text_input("Name:")
+    col2.text_input("Location:")
+
+    st.camera_input(
+        "Camera Input",
+        key="camera_input"
+    )
