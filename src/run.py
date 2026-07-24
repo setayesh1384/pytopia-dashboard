668799246a7dc31bd4d062deb190a8a76c96490a
setayesh1384 <setayesh.sedghi84@gmail.com>
2026-07-24 20:01:13 +0330

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,4 +1,22 @@
 import streamlit as st
-st.title(':zap:yyy')
-st.text(':zap:hi')
-st.text("This is text\n[and more text](that's not a Markdown link).")
+import pandas as pd
+from io import StringIO
+st.title('pytopia')
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
