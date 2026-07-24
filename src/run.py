923111c939db2b69b6459cd1798d125373fc1520
setayesh1384 <setayesh.sedghi84@gmail.com>
2026-07-25 01:43:36 +0330

    update run.py

--- src/run.py
+++ src/run.py
@@ -1,22 +1,21 @@
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 import streamlit as st
-import pandas as pd
-from io import StringIO
-st.title('pytopia')
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+st.title(':zap: Pytopia Dashboard')
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+with st.expander('Statistics'):
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+    sns.histplot(np.random.randn(100), ax=ax)
+    st.pyplot(fig)
 
-    
-    string_data = stringio.read()
-    st.write(string_data)
+with st.expander('User Profile:'):
+    col1, col2 = st.columns(2)
 
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    col1.text_input('Name:')
+    col2.text_input('Age:')
+
+    st.camera_input('Camera Input', key='camera_input')
